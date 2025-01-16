7dd4de015585d3f71f6e2996cbcfa7e8f738d599
João Vitor Bortoliero <bortoliero.10@hotmail.com>
2025-01-16 14:23:20 -0300

    Define estrutura inicial e config pom

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,18 @@
+package runners;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines({"cucumber"})
+@SelectClasspathResource("features/user.feature")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value="steps,base")
+@IncludeTags("CP001")
+public class Runner {
+
+}
